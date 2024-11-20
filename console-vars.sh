bf5a1dd634d908525c3c7e47eb1c6e9fdfe0dc0a
David Hustace <david@beaconstrategists.com>
2024-11-20 17:07:26 -0500

    more docker clean up

--- console-vars.sh
+++
@@ -1,3 +0,0 @@
-export SERVER_PORT=8080
-export AUTH_SV_ISSUER_URI=http://localhost:9081/oauth2/jwks
-export API_SVR_ENV=development
--- prod-vars.sh
+++
@@ -1,3 +0,0 @@
-export SERVER_PORT=8080
-export AUTH_SV_ISSUER_URI=http://authsvr:8080/oauth2/jwks
-export API_SVR_ENV=production
