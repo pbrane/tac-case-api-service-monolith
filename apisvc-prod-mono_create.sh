c3b7c640c4ffd112fe554a42ae148dd6ee9ad2ce
David Hustace <david@beaconstrategists.com>
2024-11-24 21:20:47 -0500

    azure container app and docker updates

---
+++ apisvc-prod-mono_create.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source ./monolith-prod.env
+source ./tac-api-version.env
+
+az containerapp create \
+  --name "$AZ_CONTAINER_NAME" \
+  --resource-group "$AZ_RESOURCE_GROUP" \
+  --environment "$AZ_CONTAINERAPP_ENV" \
+  --image "pbranestrategy/tac-case-api-service-mono:$TAC_API_VERSION" \
+  --env-vars \
+    API_ENV="$API_ENV" \
+    SERVER_PORT="$SERVER_PORT" \
+    DB_HOST="$DB_HOST" \
+    DB_PORT="$DB_PORT" \
+    API_SVR_ENV="$API_SVR_ENV" \
+    AUTH_SVR_ENV="$AUTH_SVR_ENV" \
+    AUTH_SVR_REPO="$AUTH_SVR_REPO" \
+    CLIENT_ID="$CLIENT_ID" \
+    CLIENT_SECRET="$CLIENT_SECRET" \
+    CLIENT_NAME="$CLIENT_NAME" \
+    PG_DATASOURCE_URL="$PG_DATASOURCE_URL" \
+    PG_DATASOURCE_USERNAME="$PG_DATASOURCE_USERNAME" \
+    PG_DATASOURCE_PASSWORD="$PG_DATASOURCE_PASSWORD" \
+    ACCESS_TOKEN_EXPIRATION_MIN="$ACCESS_TOKEN_EXPIRATION_MIN" \
+    REFRESH_TOKEN_EXPIRATION_MIN="$REFRESH_TOKEN_EXPIRATION_MIN" \
+  --exposed-port 8080 \
+  --target-port "$SERVER_PORT" \
+  --transport auto \
+  --ingress external \
+  --min-replicas 1 \
+  --max-replicas 3
+
